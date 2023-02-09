7ca47277f44f5f1867a4afc3eebefa9ecb1e46ce
Yogadimas <yogadimaspambudi@gmail.com>
2023-02-09 14:59:58 +0700

    20 Super Inheritance

---
+++ _20_Super_Inheritance/src/com/yogadimas/Main.java
@@ -0,0 +1,16 @@
+package com.yogadimas;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Hero hero1 = new Hero();
+        HeroStrength hero2 = new HeroStrength();
+
+        hero1.display();
+        hero2.display();
+        hero2.displaySuper();
+
+    }
+
+}
